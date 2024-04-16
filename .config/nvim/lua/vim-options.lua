8431542e45769010e2b66e1cbc6510ca5512af66
lima1909 <linke.mario@web.de>
2024-04-16 16:44:18 +0200

    format lua files and add telescope plugin

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -20,22 +20,21 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- :map <leader>s -> printed the description 'desc'
-vim.keymap.set('i', 'jk', '<Esc>', { desc = 'simulate the Esc key'})
-vim.keymap.set('n', '<Leader>s', ':write<CR>:source<CR>', { desc = 'my first test keymap'})
+vim.keymap.set('i', 'jk', '<Esc>', { desc = 'simulate the Esc key' })
+vim.keymap.set('n', '<Leader>s', ':write<CR>:source<CR>', { desc = 'my first test keymap' })
 
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 vim.api.nvim_create_user_command('MyRun', function()
-  print("Run Command")
+	print("Run Command")
 end, { nargs = '*' })
-
