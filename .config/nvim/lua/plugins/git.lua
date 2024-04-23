8f669ce619687735dba446ae91fa6ca37836e7b3
lima1909 <linke.mario@web.de>
2024-04-23 17:36:40 +0200

    add gitsigns, a git vim plugin

---
+++ .config/nvim/lua/plugins/git.lua
@@ -0,0 +1,24 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup({
+            signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+            numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+            linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
+            word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
+
+            current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+                delay = 1000,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+            },
+            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+            current_line_blame_formatter_opts = {
+                relative_time = false,
+            },
+        })
+    end,
+}
