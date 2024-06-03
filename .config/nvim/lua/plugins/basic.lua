9cfc654a73606c68d0a3bd61c8c56c572bf8bb96
lima1909 <linke.mario@web.de>
2024-06-03 20:47:26 +0200

    deactivate noise messages

--- .config/nvim/lua/plugins/basic.lua
+++ .config/nvim/lua/plugins/basic.lua
@@ -74,6 +74,16 @@ return {
         event = "VeryLazy",
         opts = {
             -- add any options here
+            messages = {
+                -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+                -- This is a current Neovim limitation.
+                enabled = false, -- enables the Noice messages UI
+                view = "messages", -- default view for messages
+                view_error = "notify", -- view for errors
+                view_warn = "messages", -- view for warnings
+                view_history = "messages", -- view for :messages
+                view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+            },
         },
         dependencies = {
             -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
