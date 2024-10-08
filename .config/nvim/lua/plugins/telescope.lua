8a8402cac880e1fd4ba77170243ddeca959d71eb
lima1909 <linke.mario@web.de>
2024-10-08 20:19:29 +0200

    add telescope for registers and grep with blob

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,7 +11,39 @@ return {
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffer" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
         vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
-        vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+        vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind Res[u]me" })
+        vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "[F]ind [R]egisters" })
         vim.keymap.set("n", "<leader>ft", "<cmd>:TodoTelescope<cr>", { desc = "[F]ind [T]odo" })
+
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader>fk",
+            [[:lua require('telescope.builtin').live_grep({ glob_pattern = vim.fn.input("Glob pattern: ") })<CR>]],
+            { noremap = true, silent = true, desc = "[F]ind [K]rep with glob" }
+        )
+
+        -- local lga_actions = require("telescope-live-grep-args.actions")
+        -- local actions = require("telescope.actions")
+        --
+        -- require("telescope").setup({
+        --     extensions = {
+        --         live_grep_args = {
+        --             auto_quoting = true, -- enable/disable auto-quoting
+        --             -- define mappings, e.g.
+        --             mappings = { -- extend mappings
+        --                 i = {
+        --                     ["<C-k>"] = lga_actions.quote_prompt(),
+        --                     ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+        --                     -- freeze the current list and start a fuzzy search in the frozen list
+        --                     ["<C-space>"] = actions.to_fuzzy_refine,
+        --                 },
+        --             },
+        --             -- ... also accepts theme settings, for example:
+        --             -- theme = "dropdown", -- use dropdown theme
+        --             -- theme = { }, -- use own theme spec
+        --             -- layout_config = { mirror=true }, -- mirror preview pane
+        --         },
+        --     },
+        -- })
     end,
 }
